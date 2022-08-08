8843b0c2c73837a06b2d5e6d3819632184b266c3
BuildTools <unconfigured@null.spigotmc.org>
2022-08-08 21:19:50 +1000

    Can move models between folders with Context menu (vert/arrows currently broken)

--- src/UIElements/ContextMenu.js
+++ src/UIElements/ContextMenu.js
@@ -1,6 +1,6 @@
 import { ClickAwayListener } from '@material-ui/core';
 import React from 'react';
-import {getFolderData,getModelData,getSelectedFolderKey,setSelectedFolderKey} from "./ContainmentTree"
+import {getFolderData,setFolderData,getModelData,getSelectedFolderKey,setSelectedFolderKey,handleModelRebase} from "./ContainmentTree"
 import {getCurrentRenderKey, setNewRenderKey, getCurrentModel, setNewModel, } from "./CanvasDraw";
 
 let rightClickedItem = "Default"; //Name of the right clicked item where "Default" is a non-object such as empty canvas space
@@ -28,12 +28,25 @@ export class ContextMenu extends React.Component {
 
     //The handle click method will check which option has been clicked and call the relevant method
     handleClick = (e) => {
+        //ignore clicks if context menu closed
         if (this.state.showMenu) {
+            //console.log(e.target.id)
             
+            //If Move model was selected, create a new context menu with available folders
+            if(e.target.id === "MoveModel"){
+                menuType = "MoveModel";
+                this.setState({showMenu: true})
+            }
+            else if(e.target.id.includes("Folder")){
+                let newFolderKey = e.target.id.replace("Folder",'')
+                //console.log(newFolderKey) 
+                handleModelRebase(rightClickedItemKey,parseInt(newFolderKey));
+                console.log("model ", rightClickedItemKey, " moved to folder id ",newFolderKey)
+                this.setState({showMenu: false})
+            }
             
-            
-            
-            this.setState({ showMenu: false });
+
+            else{this.setState({ showMenu: false });}
         }
     }
 
@@ -45,21 +58,21 @@ export class ContextMenu extends React.Component {
          we need to simulate a left click to actually select what were right clicking on in the background 
          */
         e.target.click();
-        
+
         menuType = "Default"; //reset the menu type
         rightClickedItem = "Default" //reset the selected item
         rightClickedItemKey = 0 //reset the index
 
-        console.log(e.target.className)
+        //console.log(e.target.className)
 
         //If target is tree node
         if(e.target.className === "jstree-anchor jstree-hovered jstree-clicked"){
-            console.log("clicked a tree object")
+            //console.log("clicked a tree object")
             //if target is existing folder, load the folder menu
             if(e.target.text.includes("📁")){
                 for(let folder of getFolderData()){
                     if(e.target.text === folder.text){
-                        console.log("matching folder found")
+                        //console.log("matching folder found")
                         menuType = "Folder"
                         rightClickedItem = e.target.text;
                         rightClickedItemKey = getSelectedFolderKey();
@@ -71,7 +84,7 @@ export class ContextMenu extends React.Component {
             if(e.target.text.includes("📈")){
                 for(let model of getModelData()){
                     if(e.target.text === model.text){
-                        console.log("matching model found")
+                        //console.log("matching model found")
                         menuType = "Model"
                         rightClickedItem = e.target.text;
                         rightClickedItemKey = getCurrentModel();
@@ -80,12 +93,6 @@ export class ContextMenu extends React.Component {
             }
             
         }
-
-
-
-
-
-        
         this.setState({
             xPos: `${e.pageX}px`,
             yPos: `${e.pageY}px`,
@@ -108,8 +115,7 @@ export class ContextMenu extends React.Component {
                     </div>
                 )
             }
-
-            if(menuType === "Folder"){
+            else if(menuType === "Folder"){
                 return (
 
                 //options are given classnames to identify what has been selected
@@ -118,21 +124,31 @@ export class ContextMenu extends React.Component {
                     </div>
                 )
             }
-
-            if(menuType === "Model"){
+            else if(menuType === "Model"){
                 return (
 
                 //options are given classnames to identify what has been selected
                     <div className="ContextMenu" style={{top: yPos,left: xPos,}}>
-                    <div className="CMitem">{rightClickedItem}</div>   
-                    <div className="CMitem" id="Navigate">Navigate</div>
-                    <div className="CMitem" id="Move">Move To</div>
+                    <div className="CMitem"> {rightClickedItem} </div>   
+                    <div className="CMitem" id="Navigate"> Navigate (not implemented) </div>
+                    <div className="CMitem" id="MoveModel"> Move To </div>
+                    </div>
+                )
+            }
+            else if(menuType === "MoveModel"){
+
+                let renderedOutput = getFolderData().map(item => <div className="CMitem" id={'Folder'+ item.renderKey} key={item.text}> {item.text} </div>);
+
+                return (
+
+                //options are given classnames to identify what has been selected
+                    <div className="ContextMenu" style={{top: yPos,left: xPos,}}>
+                    <div className="CMitem"> Move "{rightClickedItem}" To:</div>   
+                    <div>{renderedOutput}</div>
                     </div>
                 )
             }
         }
-
     else return null;
-
   }
 }
