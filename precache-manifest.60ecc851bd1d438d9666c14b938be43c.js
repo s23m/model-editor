13bdc5cbd4124b511cd5e58ac0df7c14ff40a166
BuildTools <unconfigured@null.spigotmc.org>
2022-08-20 12:45:22 +1000

    Updates

---
+++ precache-manifest.60ecc851bd1d438d9666c14b938be43c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d80ebdc60fa6ae3194f22718231d61",
+    "url": "/s23mGraphEditor/index.html"
+  },
+  {
+    "revision": "43a607cc8330a2f504cd",
+    "url": "/s23mGraphEditor/static/css/2.ee344228.chunk.css"
+  },
+  {
+    "revision": "d0376853908787e0aac5",
+    "url": "/s23mGraphEditor/static/css/main.7026dc36.chunk.css"
+  },
+  {
+    "revision": "43a607cc8330a2f504cd",
+    "url": "/s23mGraphEditor/static/js/2.8951af9c.chunk.js"
+  },
+  {
+    "revision": "65686f8d1bcd3be5a5a8ac576539a179",
+    "url": "/s23mGraphEditor/static/js/2.8951af9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0376853908787e0aac5",
+    "url": "/s23mGraphEditor/static/js/main.6bab6f7f.chunk.js"
+  },
+  {
+    "revision": "d44e044d32f273403938",
+    "url": "/s23mGraphEditor/static/js/runtime-main.f6ae1252.js"
+  },
+  {
+    "revision": "8ff0482838afd29afc8b6b2c2144a9b9",
+    "url": "/s23mGraphEditor/static/media/DeleteModel.8ff04828.svg"
+  },
+  {
+    "revision": "5f359781d1d8c032c4c3db484bafa8df",
+    "url": "/s23mGraphEditor/static/media/NewModel.5f359781.svg"
+  },
+  {
+    "revision": "91473c28159fc07f4fef5b47fa886b17",
+    "url": "/s23mGraphEditor/static/media/artifact.91473c28.svg"
+  },
+  {
+    "revision": "d6c49e9c74f80ea3021578da3b1c3147",
+    "url": "/s23mGraphEditor/static/media/changeFolderName.d6c49e9c.svg"
+  },
+  {
+    "revision": "ddecd45945051a2fd81baaa4281eb6fa",
+    "url": "/s23mGraphEditor/static/media/container.ddecd459.svg"
+  },
+  {
+    "revision": "5ddfbbc98ba0af223fd974b74e82799b",
+    "url": "/s23mGraphEditor/static/media/create_folder.5ddfbbc9.svg"
+  },
+  {
+    "revision": "ccd4d6e6fefd05a47644dea5ddb6e961",
+    "url": "/s23mGraphEditor/static/media/delete_folder.ccd4d6e6.svg"
+  },
+  {
+    "revision": "09e9d9dcdb53108959a59fb87cecc747",
+    "url": "/s23mGraphEditor/static/media/edge.09e9d9dc.svg"
+  },
+  {
+    "revision": "0c58ce6e959e9db2583b87b3a4e3f3c8",
+    "url": "/s23mGraphEditor/static/media/editModel.0c58ce6e.svg"
+  },
+  {
+    "revision": "8b3cad4713faf439d43a13b16dbef71e",
+    "url": "/s23mGraphEditor/static/media/select.8b3cad47.svg"
+  },
+  {
+    "revision": "3320bc8086869dc73ef14bfee4bdf907",
+    "url": "/s23mGraphEditor/static/media/specialisation.3320bc80.svg"
+  },
+  {
+    "revision": "4937cefd7250a60d620889d5f1a719fb",
+    "url": "/s23mGraphEditor/static/media/vertex.4937cefd.svg"
+  },
+  {
+    "revision": "5452d398acebefde684f878bd232a18a",
+    "url": "/s23mGraphEditor/static/media/visibility.5452d398.svg"
+  }
+]);
