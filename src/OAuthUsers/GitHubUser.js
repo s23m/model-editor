044b12f1bd3af54f8582226db6a6adc8e1d23a1a
Lee Millhouse <lee-millhouse@hotmail.com>
2023-08-15 15:48:51 +1000

    update to GithubUser class

--- src/OAuthUsers/GitHubUser.js
+++ src/OAuthUsers/GitHubUser.js
@@ -1,10 +1,16 @@
+// model-editor opens
+// click github account button
+// modal allows user to enter username and email
+// navigate button redirects to github page (to add an OAuth application)
+// user adds OAuth app, retrieves client ID and client Secret
+// user takes those values and enters them in respective field in the modal
+// retrieveproperties
 
 
 
 
-
-
-
+const GITHUB_OAUTH_APPS = 'https://github.com/settings/developers';
+const GITHUB_OAUTH_PAGE = 'https://github.com/login/oauth/authorize?client_id=';
 
 
 export class GitHubUser {
@@ -22,8 +28,26 @@ export class GitHubUser {
     this.code = code;
   }
 
+  setAccessToken(token){
+    this.accessToken = token;
+  }
+
+  navigateToSettings(){
+    window.location.assign(GITHUB_OAUTH_APPS);
+  }
+
+  loginWithGithub() {
+    window.location.assign(GITHUB_OAUTH_PAGE + this.clientId);
+  }
 }
 
-export function loginWithGithub(client) {
-  window.location.assign("https://github.com/login/oauth/authorize?client_id=" + client);
+export function retrieveProperties(){
+  const githubUser = {};
+  let fields = document.getElementsByClassName('Github-User-Fields');
+  for (let element of fields){
+
+  }
 }
+
+
+
